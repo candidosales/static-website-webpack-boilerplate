9b7819fdf334759f50098fec350f755f0bba4fae
Cândido Sales <candidosg@gmail.com>
2018-01-08 11:28:00 -0200

    remove assets unnecessary; add pwa; add favicon; add metatags of social media; responsive to mobile; optimize images;

--- webpack.common.js
+++ webpack.common.js
@@ -1,90 +1,101 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 const precss = require('precss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-   entry: {
-     app: './src/js/index.js'
-   },
-   output: {
-		 path: path.resolve(__dirname, 'dist'),
-		 filename: '[name].[chunkhash].bundle.js',
-		 chunkFilename: '[name].[chunkhash].chunk.js' 
-	 },
-	 resolve: {
-			modules: ["node_modules"],
-		},
-		module: {
-			rules: [{
-						test: /\.html$/,
-						use: 'html-loader'
-				}, {
-						test: /\.(scss)$/,
-						use: [{
-								loader: 'style-loader', // inject CSS to page
-						}, {
-								loader: 'css-loader', // translates CSS into CommonJS modules
-						}, {
-								loader: 'postcss-loader', // Run post css actions
-								options: {
-										plugins: function() { // post css plugins, can be exported to postcss.config.js
-												return [
-														precss,
-														autoprefixer
-												];
-										}
-								}
-						}, {
-								loader: 'sass-loader' // compiles Sass to CSS
-						}]
-				},
-				{
-					test: /\.(gif|png|jpe?g|svg)$/i,
-					use: [
-						'file-loader?file?name=img/[name].[ext]',
-						{
-							loader: 'image-webpack-loader',
-							options: {
-								gifsicle: {
-									interlaced: false,
-								},
-								optipng: {
-									optimizationLevel: 7,
-								},
-								pngquant: {
-									quality: '65-90',
-									speed: 4
-								},
-								mozjpeg: {
-									progressive: true,
-									quality: 65
-								},
-								// Specifying webp here will create a WEBP version of your JPG/PNG images
-								webp: {
-									quality: 75
-								}
-							},
-						},
-					],
-				},
+    entry: {
+        app: './src/js/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].bundle.js',
+        chunkFilename: '[name].[chunkhash].chunk.js'
+    },
+    resolve: {
+        modules: ["node_modules"],
+    },
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: 'html-loader'
+            }, {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function() { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                precss,
+                                autoprefixer
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader?file?name=img/[name].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // Specifying webp here will create a WEBP version of your JPG/PNG images
+                            webp: {
+                                quality: 75
+                            }
+                        },
+                    },
+                ],
+            },
 
-				/* File loader for supporting fonts, for example, in CSS files.
-				*/
-				{
-						test: /\.(eot|woff2?|ttf)([\?]?.*)$/,
-						use: 'file-loader'
-				}
-			]
-		}, 
-		plugins: [
-			// new webpack.NoEmitOnErrorsPlugin(),
-			new CleanWebpackPlugin(['./dist/*']),
-			new HtmlWebpackPlugin({
-			 title: 'MoneyEx - Embrace the Future - São Paulo',					
-			 template: './src/index.html'     
-		 })
-		]
+            /* File loader for supporting fonts, for example, in CSS files.
+             */
+            {
+                test: /\.(eot|woff2?|ttf)([\?]?.*)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.NoEmitOnErrorsPlugin(),
+        new CleanWebpackPlugin(['./dist/*']),
+        new HtmlWebpackPlugin({
+            title: 'MoneyEx - Embrace the Future - São Paulo',
+            template: './src/index.html'
+        }),
+        new WebpackPwaManifest({
+            name: 'MoneyEx - Embrace the Future',
+            short_name: 'MoneyEx',
+            description: 'Driving People, Technology, Finance and Design to Build an Exponencial Future.',
+            background_color: '#134a6e',
+            icons: [{
+                src: path.resolve('src/img/logo-1024x1024.png'),
+                sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            }]
+        })
+    ]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,78 +4,74 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	module: {
-				rules: [
-					// {
-					// 	test: /\.css$/,
-					// 	use: ExtractTextPlugin.extract({
-					// 		fallback: 'style-loader',
-					// 		use: [
-					// 			{ loader: 'css-loader', options: { minimize: true } }
-					// 		]
-					// 	})
-					// },
-					{
-						test: /\.*(sass|scss)$/,
-						use: ExtractTextPlugin.extract({
-							fallback: 'style-loader',
-							use: [
-								{ loader: 'css-loader', options: { minimize: true } },
-								{ loader: 'sass-loader'}
-							]
-						})
-					},
-				]
-		},
-   plugins: [
-		new HtmlWebpackPlugin({
-			title: 'MoneyEx - Embrace the Future - São Paulo',					
-			template: './src/index.html',
-			// minify: {
-			// 	caseSensitive: true,
-			// 	collapseWhitespace: true,
-			// 	keepClosingSlash: true
-			// }
-		}),
-		new ExtractTextPlugin('[name].[contenthash].css'),		
-		 new UglifyJSPlugin({
-			sourceMap: false,
-			uglifyOptions: {
-				output: {
-					comments: false,
-					beautify: false
-				}
-			}
-		}),
-		 new CompressionPlugin({
-				asset: '[path].gz[query]',
-				algorithm: 'gzip',
-				test: /\.(css|js|html)$/,
-				threshold: 10240,
-				minRatio: 0.8
-		}),
-		new CopyWebpackPlugin([{
-			from: './src/manifest.json'
-		}, {
-				from: './src/manifest.webapp'
-		}, {
-				from: './src/robots.txt'
-		}, {
-				from: './src/favicon.ico'
-		}]),
-		new ManifestPlugin()
-	 ],
-	 node: {
-		global: true,
-		crypto: 'empty',
-		process: true,
-		module: false,
-		clearImmediate: false,
-		setImmediate: false
-	 }
+    module: {
+        rules: [
+            // {
+            // 	test: /\.css$/,
+            // 	use: ExtractTextPlugin.extract({
+            // 		fallback: 'style-loader',
+            // 		use: [
+            // 			{ loader: 'css-loader', options: { minimize: true } }
+            // 		]
+            // 	})
+            // },
+            {
+                test: /\.*(sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { minimize: true } },
+                        { loader: 'sass-loader' }
+                    ]
+                })
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'MoneyEx - Embrace the Future - São Paulo',
+            template: './src/index.html',
+            // minify: {
+            // 	caseSensitive: true,
+            // 	collapseWhitespace: true,
+            // 	keepClosingSlash: true
+            // }
+        }),
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        new UglifyJSPlugin({
+            sourceMap: false,
+            uglifyOptions: {
+                output: {
+                    comments: false,
+                    beautify: false
+                }
+            }
+        }),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(css|js|html)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/robots.txt'
+        }, {
+            from: './src/favicon.ico'
+        }, {
+            from: './src/img/metadata.png'
+        }])
+    ],
+    node: {
+        global: true,
+        crypto: 'empty',
+        process: true,
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+    }
 });
